47600f53c1b34455419f5119190eb5dda1372ecf
Safal <080bei034.safal@pcampus.edu.np>
2025-06-28 11:11:10 +0545

    Simple working testbench

--- Projects/FIFO_Buffer/design.v
+++ Projects/FIFO_Buffer/design.v
@@ -2,14 +2,14 @@ module FIFO (
     input  logic clk,rst,wr_en,rd_en,
     input  logic [7:0] din,
     output logic full, empty,
-    output bit [7:0] dout
+    output logic [7:0] dout
 );
     logic [7:0] mem[0:1023];
     logic [15:0] count;
 
-    parameter one_kb = 10'd1024;
-    empty = (!count) ? 1'b1 : 1'b0;
-    full =  (count == one_kb) ? 1'b1 : 1'b0;
+    parameter one_kb = 10'd1023;
+    assign empty = (!count) ? 1'b1 : 1'b0;
+    assign full =  (count == one_kb) ? 1'b1 : 1'b0;
 
     logic [15:0] wr_ptr , rd_ptr ;
 
@@ -22,11 +22,14 @@ module FIFO (
             count <= 16'b0;
             wr_ptr <= 16'b0;
             rd_ptr <= 16'b0;
-            empty <= 1'b0;
-            full <= 1'b0;
+            dout <= 8'bz; //Buffer in high impedance after each reset.
+            $display("Inside rst"); //for debugginf purpose
+            
 
         end else if(rst) begin 
+            //Write Operation
             if(wr_en && !full) begin 
+                $display("Inside write"); //for debugginf purpose
                 mem[wr_ptr] <= din;
                 if(wr_ptr == 16'd1023)
                     wr_ptr <= 16'b0;
@@ -36,16 +39,18 @@ module FIFO (
                 count <= count + 1;              
             end
             
+            //Read Operation
             if(rd_en && !empty) begin 
+                $display("Inside read");  //for debugginf purpose
                 dout <= mem[rd_ptr];
+                mem[rd_ptr] <= 8'b0;
                 if(rd_ptr == 16'd1023)
                     rd_ptr <= 16'b0; 
                 else 
                     rd_ptr <= rd_ptr + 1;                                            
             end
             if(!rd_en) begin 
-                dout <= 8'b0;
-                
+                dout <= 8'bz; //if the buffer is not in the read mode then the output of the buffer should be in high impedance
             end
         end
     end
--- Projects/FIFO_Buffer/test_FIFO.py
+++ Projects/FIFO_Buffer/test_FIFO.py
@@ -9,11 +9,11 @@ import random
 async def FIFO_test(dut):
     a = [1,0]
     b = [0,1]
-    c = [0,12]
+    c = ["zzzzzzzz",12]
 
-    cocotb.start_soon(Clock(dut.clk, 10, units="ns").start())
+    cocotb.start_soon(Clock(dut.clk, 2, units="ns").start())
     dut.rst.value = 0
-    await Timer(20, units="ns")
+    await Timer(5, units="ns")
     dut.rst.value = 1
     await RisingEdge(dut.clk)
 
@@ -23,8 +23,8 @@ async def FIFO_test(dut):
         dut.wr_en.value = a[i]
         await Timer(5, units = "ns")
         out = dut.dout.value
-        print(f"{i} Wd en {dut.wr_en.value} | Rd en {dut.rd_en.value}|Count {dut.count.value}|Val {out} | Full {dut.full.value} | Empty {dut.empty.value} | wr_ptr {dut.wr_ptr.value} | rd_ptr {dut.rd_ptr.value} | rst {dut.rst.value} | clk {dut.clk.value}")
-        assert c[i] == out, f"Out mismatach expected {c[i]} got {out}"
+        print(f"{i} Wd en {dut.wr_en.value} |din {dut.din.value}| Rd en {dut.rd_en.value}|Count {dut.count.value}|Val {out} | Full {dut.full.value} | Empty {dut.empty.value} | wr_ptr {dut.wr_ptr.value} | rd_ptr {dut.rd_ptr.value} | rst {dut.rst.value} | clk {dut.clk.value}")
+        assert c[i] == out, f"{i} Out mismatach expected {c[i]} got {out}"
 
         
 
